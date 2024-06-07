7c69b29006f1215be087891ec94ed1a06c01244b
Adam Raschke <araschke@asu.edu>
2024-06-06 21:24:48 -0700

    homepage editing

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,36 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Box, VStack, Center, HStack, Spacer } from '@chakra-ui/react';
+import { Box, VStack, Center, HStack, Spacer, Grid } from '@chakra-ui/react';
 
 const Header = ({ username }) => (
+
   <React.Fragment>
     <VStack
       borderBottom={'1px'} borderColor="gray.300"
       w="100%" h="100px" bg="gray.200">
-      <HStack w="100%" h="100%" p="1">
-        <Box w="400px" h="100%" bg="pink" rounded="md">
+      
+      <Box w="400px" h="100%" bg="pink" rounded="md">
           <Center h="100%">
             BiteWise
           </Center>
         </Box>
+      <HStack w="100%" h="100%" p="1">
+        
+        
         <Spacer />
-        <Box w="150px" h="100%" align="end" pr="5">
-          <Link as={Link} to="/" >
-            Home
-          </Link>
-          {username ? (
-            <Link to="/profile">{username}</Link>
-          ) : (
-            <>
-              <Link as={Link} to="/login" >
-                Log in
+        
+        <Grid templateColumns='repeat(3, 1fr)'gap={1} >
+
+          <Box w="70%" h="100%" bg="pink" rounded="md">
+            <Center h="100%">
+              <Link as={Link} to="/" >
+                Home
               </Link>
-              <Link as={Link} to="/SignUp">
-                Sign Up
+            </Center>
+          </Box>
+
+          <Grid templateColumns='repeat(2, 1fr)'gap={1} >
+            {username ? (
+              <Link to="/profile">
+                {username}
               </Link>
-            </>
-          )}
-        </Box>
+            ) : (
+              <>
+                <Box w="100%" h="100%" bg="pink" rounded="md">
+                  <Center h="100%">
+                    <Link as={Link} to="/login" >
+                      Log in
+                    </Link>
+                  </Center>
+                </Box>
+
+                <Box w="100%" h="100%" bg="pink" rounded="md">
+
+                  <Center h="100%">
+                    <Link as={Link} to="/SignUp">
+                      Sign Up
+                    </Link>
+                  </Center>
+                </Box>
+              </>
+            )}
+          </Grid>
+        </Grid>
       </HStack>
     </VStack>
   </React.Fragment>
--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -4,7 +4,7 @@ import { Text, VStack } from '@chakra-ui/react';
 
 const Home = () => (
   <React.Fragment>
-    <VStack h='calc(100vh - 200px)'>
+    <VStack h='calc(100vh - 200px)' bg="orange.100">
       <Text>Welcome to BiteWise</Text>
       <Text>Our Mission: To ensure great food, generous portions, and reasonable prices.</Text>
       <RestaurantList />
