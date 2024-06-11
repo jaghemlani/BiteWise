2f4cba4286421270b441abb99af7f297b218ebcc
Adam Raschke <araschke@asu.edu>
2024-06-10 18:40:48 -0700

    dist edit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/BiteWise/",
+  base: "https://github.com/jaghemlani/BiteWise",
   plugins: [react()],
 })
